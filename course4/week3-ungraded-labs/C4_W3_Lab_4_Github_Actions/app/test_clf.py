7898678b277d2f25b0be23269d8b6d9e4bc26361
allan5050 <58089881+allan5050@users.noreply.github.com>
2023-07-22 21:16:21 +0300

    adding new classifier with scaling

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,7 +1,24 @@
 import pickle
 from main import clf
 
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
+
+# unit test
+def test_pipeline_and_scaler():
+
+    # Check if clf is an instance of sklearn.pipeline.Pipeline 
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+    
+    if isPipeline:
+        # Check if first step of pipeline is an instance of 
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
+
+# unit test
 def test_accuracy():
 
     # Load test data
@@ -16,3 +33,4 @@ def test_accuracy():
 
     # Accuracy should be over 90%
     assert acc > 0.9
+
